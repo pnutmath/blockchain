e33a2da139a18fb46e8cdeb996dcf3fe84da2cca
pnutmath <abgawade24@gmail.com>
2021-11-20 12:15:13 +0530

    👔 added Blockchain class

--- block.test.js
+++ block.test.js
@@ -20,40 +20,40 @@ describe('Block', () => {
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
   });
-});
 
-describe('genesis()', () => {
-  const genesisBlock = Block.genesis();
+  describe('genesis()', () => {
+    const genesisBlock = Block.genesis();
 
-  it('returns a Block instance', () => {
-    expect(genesisBlock instanceof Block).toBe(true);
+    it('returns a Block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+    it('returns the genesis data instance', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
   });
-  it('returns the genesis data instance', () => {
-    expect(genesisBlock).toEqual(GENESIS_DATA);
-  });
-});
 
-describe('mineBlock()', () => {
-  const lastBlock = Block.genesis();
-  const data = 'mined data';
+  describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'mined data';
 
-  const minedBlock = Block.mineBlock({ lastBlock, data });
+    const minedBlock = Block.mineBlock({ lastBlock, data });
 
-  it('returns a Block instance', () => {
-    expect(minedBlock instanceof Block).toBe(true);
-  });
-  it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
-    expect(minedBlock.lastHash).toEqual(lastBlock.hash);
-  });
-  it('sets the `data`', () => {
-    expect(minedBlock.data).toEqual(data);
-  });
-  it('sets the `timestamp`', () => {
-    expect(minedBlock.timestamp).not.toEqual(undefined);
-  });
-  it('created a SHA-256 `hash` based on the proper inputs', () => {
-    expect(minedBlock.hash).toEqual(
-      cryptoHash(minedBlock.timestamp, lastBlock.hash, data)
-    );
+    it('returns a Block instance', () => {
+      expect(minedBlock instanceof Block).toBe(true);
+    });
+    it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    });
+    it('sets the `data`', () => {
+      expect(minedBlock.data).toEqual(data);
+    });
+    it('sets the `timestamp`', () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
+    it('created a SHA-256 `hash` based on the proper inputs', () => {
+      expect(minedBlock.hash).toEqual(
+        cryptoHash(minedBlock.timestamp, lastBlock.hash, data)
+      );
+    });
   });
 });
